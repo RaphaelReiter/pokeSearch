ff21ff0dd3563dd32a86270cd49f8a7a21b071c1
RaphaelReiter <rdso.reiter@gmail.com>
2017-06-08 18:04:08 +0200

    first commit

---
+++ pokesearch/pokesearch-bridging-header.h
@@ -0,0 +1,10 @@
+//
+//  pokesearch-bridging-header.h
+//  pokesearch
+//
+//  Created by Raphael Reiter on 29/05/2017.
+//  Copyright © 2017 Raphael Reiter. All rights reserved.
+//
+
+
+#import "GeoFire.h"
---
+++ pokesearch/pokesearch.h
@@ -0,0 +1,13 @@
+//
+//  pokesearch.h
+//  pokesearch
+//
+//  Created by Raphael Reiter on 29/05/2017.
+//  Copyright © 2017 Raphael Reiter. All rights reserved.
+//
+
+#ifndef pokesearch_h
+#define pokesearch_h
+
+
+#endif /* pokesearch_h */
